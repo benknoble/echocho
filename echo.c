3050c2d253faa74deefe2d60d634f3e584b32d33
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-08-26 00:49:04 -0400

    echo.c: implement echo and echo_n

--- echo.c
+++ echo.c
@@ -1,3 +1,25 @@
 #include "echo.h"
+#include <stdio.h>
 
+int print_args(int argc, char **argv) {
+    int err = 0;
+    for (int i = 0; i < argc-1; ++i) {
+        err = printf("%s ", argv[i]);
+        if (err < 0) return err;
+    }
+    err = printf("%s", argv[argc-1]);
+    if (err < 0) return err;
+    return 0;
+}
 
+int echo(int argc, char **argv) {
+    int err = print_args(argc, argv);
+    if (err < 0) return err;
+    err = printf("\n");
+    if (err < 0) return err;
+    return 0;
+}
+
+int echo_n(int argc, char **argv) {
+    return print_args(argc, argv);
+}
